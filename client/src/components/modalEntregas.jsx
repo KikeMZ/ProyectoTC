83d8f4774b5dc16777a6931a94857938693500eb
PhoenixPW <bros_z1@hotmail.com>
2024-04-14 20:57:48 -0600

    Se agrego la seccion de entregas a la vista del profesor

---
+++ client/src/components/modalEntregas.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Modal, ModalHeader, ModalBody, ModalContent, ModalFooter, useDisclosure, Button, Card, CardHeader, CardBody, Divider } from "@nextui-org/react";
+import { Link } from "react-router-dom";
+import { claseContext } from "../layouts/layoutProfesor";
+import { getCriteriosByNRC } from "../services/claseCriterio.api";
+
+import {Select, SelectSection, SelectItem} from "@nextui-org/react";
+import { MdOutlineLibraryAdd } from "react-icons/md";
+
+
+export default function ModalEntregas({ controlModal, setEntregas, nrc}) {
+  const { datosClase } = useContext(claseContext)
+  const [ criterios, setCriterios ] = useState([]);
+  const [ nombreEntrega, setNombreEntrega ] = useState("");
+  const [ tipo, setTipo ] = useState(0); 
+
+
+  const crearEntrega = () =>{
+   let entregas = {
+    "id":-1,
+    "nombre": nombreEntrega,
+    "tipo": tipo,
+   }
+   
+   setEntregas( (listaEntregas) => [...listaEntregas, entregas]);
+   //setMaximo( (maximo) => maximo + ponderacion);
+  }
+
+  useEffect( ()=>{
+
+    const obtenerCriterios = async () =>{
+
+      let listaCriterios = await getCriteriosByNRC(nrc);
+      setCriterios(listaCriterios.data);
+
+    }
+
+    obtenerCriterios();
+  }, []);
+
+  return (  
+    <Modal classNames={{closeButton:"text-foreground-white text-2xl hover:bg-black active:text-black"}} isOpen={controlModal.isOpen} onOpenChange={controlModal.onOpenChange} >
+    <ModalContent>
+     {
+      (onClose) => (
+       <>
+       <ModalHeader className="bg-gradient-to-tr from-primary-100 to-primary-200 text-xl text-white font-bold">
+       <MdOutlineLibraryAdd size="28px" className="mr-2"/>
+
+        Crear entrega
+       </ModalHeader>
+       <ModalBody className="gap-1 text-black">
+        
+         <label htmlFor="nombreE" className=" font-semibold mt-3">Nombre</label>
+         <input id="nombreE" className="px-2 border-2 border-black rounded" onChange={(e) => setNombreEntrega( e.target.value)} required/>
+
+         <label htmlFor="nombreP" className="font-semibold mt-2">Tipo</label>
+         <Select onChange={(e) => setTipo( e.target.value)} isRequired radius="sm" classNames={{ label:"text-black"}} variant="bordered" labelPlacement={"outside"} placeholder="Seleccione un criterio" aria-label="Tipo">
+          {
+           criterios.map( (c) => (
+            <SelectItem key={c.id} value={c.id} className="text-black text-2xl">
+             {c.criterio_detail.nombre}
+            </SelectItem>
+           ))
+          }
+
+         </Select>
+
+       </ModalBody>
+       <ModalFooter>
+        <Button color="danger" style={{fontWeight:"bold"}} onPress={onClose}>Cancelar</Button>
+        <Button type="submit" color="success" style={{background:"green",color:"white" ,fontWeight:"bold"}} onPress={() => {crearEntrega(); onClose(); }}>Crear</Button>
+       </ModalFooter>
+       </>
+      )
+     }
+    </ModalContent>
+    </Modal>
+);
+}
--- client/src/pages/entregas.jsx
+++ client/src/pages/entregas.jsx
@@ -1,11 +1,22 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useState,  useEffect } from "react";
 import { NavContext } from "../layouts/layoutProfesor";
+import { claseContext } from "../layouts/layoutProfesor";
+import ModalEntregas from "../components/modalEntregas";
+
+
+import { Input, Card, CardBody, Button, useDisclosure } from "@nextui-org/react";
+import toast from 'react-hot-toast';
 
 
 
 const Entregas = () => {
 
-  const { showNav, shownav } = useContext(NavContext); 
+  const { showNav, shownav } = useContext(NavContext);
+  const { dataClase } = useContext(claseContext);
+  const controlModal = useDisclosure();
+  
+  const [ entregas, setEntregas ] = useState([]);
+  const [ mostrarEntregas, setMostrarEntregas ] = useState(false);
 
     useEffect(() => {
         showNav();
@@ -13,11 +24,30 @@ const Entregas = () => {
     
     console.log(shownav)
 
+  
     
   return (
-    <div>Entregas 
-    </div>
-  )
+    <>
+    <div className="flex flex-col items-center justify-start min-h-full">
+    <div className="flex flex-col items-center justify-center">
+          <h1 className="text-center text-3xl font-bold mt-6 mb-4">Parece que aun no hay ninguna entrega para esta clase.</h1>
+          <div>
+          <Button
+              radius="large"
+              className="bg-gradient-to-tr from-primary-100 to-primary-200 text-white px-6 py-6 mt-2 mr-3 mb-10 font-bold text-base"
+              onClick={controlModal.onOpen}
+          >
+              <i className="pi pi-plus" style={{fontSize:"16px",fontWeight:"bold"}}></i> Crear entrega
+          </Button>
+
+          </div>
+
+
+      </div>
+      </div>
+      <ModalEntregas controlModal={controlModal} setEntregas={setEntregas} nrc={dataClase.nrc}></ModalEntregas>
+      </>
+)
 }
 
 export default Entregas
